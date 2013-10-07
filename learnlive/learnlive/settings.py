20fe5907e39929bcc1e0cd5b63f1ce8de9a7cd30
xonecas <seancaetanomartin@gmail.com>
2013-10-07 16:45:52 -0400

    updated settings to have some auth login page, and urls for login

--- learnlive/learnlive/settings.py
+++ learnlive/learnlive/settings.py
@@ -131,6 +131,9 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
 )
 
+# LOGIN URL DEFAULT
+LOGIN_URL = '/login/'
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
