8eb5d56c46056af51da7dd23671576f2147b3adf
Aditya Agarwalla <Aditya_MINI@nat-oitwireless-inside-vapornet100-d-19052.princeton.edu>
2013-11-16 18:24:56 -0500

    Search page and bid profile page

--- learnlive/learnlive/query_parser/urls.py
+++ learnlive/learnlive/query_parser/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from learnlive.query_parser import views
 
 urlpatterns = patterns('',
+		url(r'^search/$', views.AskSearchView.as_view(), name='search'),
         url(r'^$', views.AskQueryView.as_view(), name='query'),
    )
