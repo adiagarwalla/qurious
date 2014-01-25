2a6d4460e219ace986a33c02fcfa9810b37ea63d
Abhinav Khanna <akhanna@princeton.edu>
2014-01-25 10:56:23 -0500

    updated the inclass model to have some of the new stuff, next step is to add the logic that will generate this on the user side

--- learnlive/learnlive/inclass/models.py
+++ learnlive/learnlive/inclass/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from learnlive.auth.models import UserProfile
 
 # Create your models here.
+class Session(models.Model):
+    """
+    This is a session object, it contains information regarding a new
+    in class session. This information is what allows someone to gain
+    access to the url that grants entry into the session.
+    """
+    id_tutor = models.ForeignKey(UserProfile)
+    id_user = models.ForeignKey(UserProfile)
+    time = models.IntegerField()
+    session_key = models.TextField()
+
+class InClassNotification(models.Model):
+    """
+    This is the notification object that is used to create and generate
+    an in class notification.
+    """
+    id_from = models.ForeignKey(UserProfile)
+    message = models.TextField()
+    m_type = models.IntegerField() # This will have to be carefully done
+    url_inclass = models.CharField(max_length=512)
+
