bd81a072098ee1f62de1f75b2b53773e153aa0c1
Abhinav Khanna <akhanna@princeton.edu>
2014-01-26 00:09:46 -0500

    updated incorrectly

---
+++ learnlive/learnlive/bid_platform/migrations/0002_initial.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Skill'
+        db.create_table(u'bid_platform_skill', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=512)),
+        ))
+        db.send_create_signal(u'bid_platform', ['Skill'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Skill'
+        db.delete_table(u'bid_platform_skill')
+
+
+    models = {
+        u'bid_platform.skill': {
+            'Meta': {'object_name': 'Skill'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '512'})
+        }
+    }
+
+    complete_apps = ['bid_platform']
