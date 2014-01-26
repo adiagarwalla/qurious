59ff8bb680a589d1739ca9848ca7b09aa410d2a6
Abhinav Khanna <akhanna@princeton.edu>
2014-01-26 00:05:15 -0500

    updated to have a more complete system, NOT TESTED

--- learnlive/learnlive/inclass/urls.py
+++ learnlive/learnlive/inclass/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from learnlive.inclass import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.InClassView.as_view(), name='in-class'),
+        url(r'^(?P<id_tutor>\w+)/(?P<id_user>\w+)/(?P<session_id>\w+)/(?P<sign>\w+)/$', views.InClassView.as_view(), name='in-class'),
+        url(r'^$', views.InClassView.as_view(), name='in-class-post'),
    )
--- learnlive/learnlive/inclass/views.py
+++ learnlive/learnlive/inclass/views.py
@@ -15,6 +15,22 @@ from learnlive.inclass.models import InClassNotification
 class InClassView(View):
 
     def get(self, request, *args, **kwargs):
+        import pdb; pdb.set_trace()
+        id_tutor = self.kwargs['id_tutor']
+        id_user = self.kwargs['id_user']
+        sign = self.kwargs['sign']
+        session_id = self.kwargs['session_id']
+        rsa = RSA.objects.filter()[0]
+        rsa_public = RSA.importKey(rsa.public)
+        message = "" + id_tutor + "" + id_user + "" + session_id
+        if not rsa_public.verify(message, (sign, "")):
+            return redirect("/")
+
+        # proceed to ensure that it is still a valid session
+        sess = Session.objects.get(id=session_id)
+        if sess.is_cancelled == True:
+            return redirect("/")
+
         token = generate_token("1_MX40NDU5NDk3Mn5-U2F0IEphbiAyNSAxNDozMDo1OCBQU1QgMjAxNH4wLjM3OTI0ODJ-")
         data = {
                  'token': token,
@@ -25,14 +41,15 @@ class InClassView(View):
         # generates the signature that will be used to create the proper url
         rsa = RSA.objects.filter()[0];
         rsa_private = RSA.importKey(rsa.private)
-        (sign, random) = rsa_private.sign("" + id_tutor + "" + id_user)
-        return "/" + id_tutor + "/" + id_user + "/" + sign + "/"
+        (sign, random) = rsa_private.sign("" + id_tutor + "" + id_user + "" + session_id)
+        return "/" + id_tutor + "/" + id_user + "/" + session_id + "/" + sign + "/"
 
 
     def post(self, request, *args, **kwargs):
         # This method will generate the session object corresponding to this session
         # it will then generate the notifcation to the tutor and redirect the user to
         # the proper in class page.
+        import pdb; pdb.set_trace()
         form = CreateSessionForm(request.POST)
         if form.is_valid():
             # we wanna create the session object here
@@ -48,3 +65,6 @@ class InClassView(View):
             # generate a notification object and attach it to the tutor
             notification = InClassNotification(prof_from=user, message="Requesting a tutor session with you!", prof_to=tutor, url_inclass=url)
             notification.save()
+            return redirect(url)
+        else:
+            return redirect('/')
