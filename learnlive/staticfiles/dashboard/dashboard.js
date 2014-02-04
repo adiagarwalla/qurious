1e1e71985dcd4bbec624e03349607314266e5730
Abhinav Khanna <akhanna@princeton.edu>
2014-02-04 09:20:47 -0500

    added static files

---
+++ learnlive/staticfiles/dashboard/dashboard.js
@@ -0,0 +1,41 @@
+function get_user_skills() {
+    // this function gets the user skills
+    $.get('/dashboard/skills/', function(data) {
+        // alert the data for now
+        $("#skill_set0").empty();
+        $("#skill_set1").empty();
+        $("#skill_set2").empty();
+        for (i = 0; i < data.length; i++) {
+            var mod = i % 3;
+            if (!data[i].fields.is_marketable) {
+                $("#skill_set" + mod).append("<li class=\"active\"><input type=\"hidden\" value=\""+ data[i].pk +"\" /><a href=\"#\" data-toggle=\"modal\" data-target=\"#myModal\" class=\"modal_link\"><span class=\"badge pull-right\">" + data[i].fields.price + "</span>" + data[i].fields.name + "</a></li>");
+            } else {
+                // alternative html goes here.
+                $("#skill_set" + mod).append("<li class=\"active\"><input type=\"hidden\" value=\""+ data[i].pk +"\" /><a href=\"#\" data-toggle=\"modal\" data-target=\"#myModal\" style=\" background-color: rgb(201,62,59);\"><span class=\"badge pull-right\">" + data[i].fields.price + "</span>" + data[i].fields.name + "</a></li>");
+            }
+        }
+    });
+}
+
+function get_profile() {
+    // gets the profile of the person and autopopulates the values
+    // of the input fields
+    $.get('/dashboard/profile/', function(data) {
+        for (i = 0; i < data.length; i++) {
+            $("#profile_name").val(data[i].fields.profile_name);
+            $("#Desc").val(data[i].fields.bio);
+        }
+    });
+}
+
+function get_notifications() {
+    // gets the dashboard data
+    $.get('/dashboard/notifications/', function(data) {
+        $("#notification").empty();
+        for (i = 0; i < data.length; i++) {
+            $("#notification").prepend("<div class=\"alert alert-info\">" + data[i].fields.message + " - " + data[i].fields.prof_from_username + "<a href=\"" + data[i].fields.url_inclass + "\" class=\"btn btn-primary active\" role=\"button\" style=\" float: right; margin-top: -7px;\">Enter into session</a></div>");
+        }
+        $("#notification").prepend("<h2 class=\"tabheading\">Notification</h2>");
+    });
+}
+
