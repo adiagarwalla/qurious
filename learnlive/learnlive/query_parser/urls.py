b8437c1926133b98b985948995c5a1cf8b0ada2f
Abhinav Khanna <akhanna@princeton.edu>
2014-02-23 16:32:47 -0500

    fixed one cache error
    Former-commit-id: 134eeb94b2b8712b788b76e9b7dc21aa36c406f7

--- learnlive/learnlive/query_parser/urls.py
+++ learnlive/learnlive/query_parser/urls.py
@@ -4,7 +4,7 @@ from learnlive.query_parser import views
 from django.views.decorators.cache import cache_page
 
 urlpatterns = patterns('',
-        url(r'^$', cache_page(300 * 60)(views.AskSearchView.as_view()), name='search'),
+        url(r'^$', cache_page(10)(views.AskSearchView.as_view()), name='search'),
         url(r'^v2/$', cache_page(60)(views.AlternateSearchView.as_view()), name='alt'),
         url(r'^search/$', views.ProcessSearchView.as_view(), name='process-search'),
         url(r'^profile/$', views.ProfileView.as_view(), name='profile'),
